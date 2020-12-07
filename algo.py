bfa7202ede3386f2ee6883bba36a16be773c9739
benjaminbransten <branstenjamin@gmail.com>
2020-12-07 19:26:50 +0100

    sauvegarde

---
+++ algo.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from pandas_datareader import data, wb
+import datetime
+import numpy as np
+from scipy import sparse
+import matplotlib.pyplot as plt
+from IPython.display import display
+from sklearn.model_selection import train_test_split
+import copy
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 # MLtrading
-# MLTRADING
+nouveau
