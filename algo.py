9ecfe48ac49347e34ea3899018ce08abb70cfbe2
benjaminbransten <branstenjamin@gmail.com>
2020-12-07 23:28:34 +0100

    train test

--- algo.py
+++ algo.py
@@ -7,3 +7,34 @@ import matplotlib.pyplot as plt
 from IPython.display import display
 from sklearn.model_selection import train_test_split
 import copy
+
+start = pd.to_datetime('2015-01-01')
+end = pd.to_datetime('today')
+
+df = data.DataReader('FB', 'yahoo', start , end)
+
+open=df['Open']
+
+def data(day_one,col):
+    L=[]
+    n=col.shape[0]
+    for k in range (day_one,n):
+        M=[]
+        for j in range(1,day_one+1):
+            M.append(col[k-j])
+        L.append(M)
+    X=np.array(L)
+    return X
+
+def target (day_one,col):
+    L=[]
+    n=col.shape[0]
+    for k in range (day_one,n):
+        L.append(col[k])
+    y=np.array(L)
+    return y
+
+X=data(900,open)
+y= target (900,open)
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0)
